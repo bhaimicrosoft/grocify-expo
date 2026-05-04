f78c6378323086723df6c4ec055d05fbf6c4cada
Indranil Mukherjee <imukherjee@microsoft.com>
2026-05-04 18:15:04 +0530

    set up Tailwind CSS with NativeWind integration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,9 +2,11 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to NativeWind!
+        </Text>
+      </View>
   );
 }
 
